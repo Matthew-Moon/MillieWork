46dca7042ae545912d8c23599f198a92bcd57442
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-05-11 02:36:28 +0900

    feat: 뷰모델 데이터 고양이 데이터 불러오기 구현

--- app/src/main/java/com/millie/android/presentation/viewmodel/CatViewModel.kt
+++ app/src/main/java/com/millie/android/presentation/viewmodel/CatViewModel.kt
@@ -2,9 +2,13 @@ package com.millie.android.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.millie.android.domain.model.CatImage
 import com.millie.android.domain.usecase.GetImagesSearchUseCase
 import com.millie.android.presentation.state.CatUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -16,19 +20,8 @@ class CatViewModel @Inject constructor(
     val getImagesSearchUseCase: GetImagesSearchUseCase
 ) : ViewModel() {
 
-    private val _uiState = MutableStateFlow<CatUiState>(CatUiState.Loading)
-    val uiState: StateFlow<CatUiState> = _uiState
+    val catData: Flow<PagingData<CatImage>> =
+        getImagesSearchUseCase()
+            .cachedIn(viewModelScope)
 
-    fun getImagesSearch() {
-        viewModelScope.launch {
-            _uiState.value = CatUiState.Loading
-            try {
-                val result = getImagesSearchUseCase()
-                Timber.d("getImagesSearch result = $result")
-                _uiState.value = CatUiState.Success(result)
-            } catch (e: Exception) {
-                Timber.e("getImagesSearch result = ${e.message}")
-            }
-        }
-    }
 }

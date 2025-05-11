cf33071585f1eefcf9b267909bb74516f2a6a9f0
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-05-11 19:16:06 +0900

    feat: 테이블 수정 및 마이그레이션 코드 추가

--- app/src/main/java/com/millie/android/di/DatabaseModule.kt
+++ app/src/main/java/com/millie/android/di/DatabaseModule.kt
@@ -2,6 +2,8 @@ package com.millie.android.di
 
 import android.content.Context
 import androidx.room.Room
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.millie.android.data.db.CatDao
 import com.millie.android.data.db.CatDatabase
 import dagger.Module
@@ -22,9 +24,43 @@ object DatabaseModule {
             context = context,
             klass = CatDatabase::class.java,
             name = "cat_db"
-        ).build()
+        )
+            .addMigrations(MIGRATION_2_3)
+            .build()
     }
 
     @Provides
     fun provideCatDao(database: CatDatabase): CatDao = database.catDao()
+}
+
+val MIGRATION_1_2 = object : Migration(1, 2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL(
+            """
+            ALTER TABLE cat_images
+            ADD COLUMN localImagePath TEXT
+        """.trimIndent()
+        )
+    }
+}
+
+val MIGRATION_2_3 = object : Migration(2, 3) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        // DROP COLUMN을 미지원
+
+        // 1. 기존 테이블 제거
+        db.execSQL("DROP TABLE cat_images")
+
+        // 2. localImagePath 컬럼 제거후 재생성
+        db.execSQL(
+            """
+            CREATE TABLE cat_images (
+                id TEXT PRIMARY KEY NOT NULL,
+                url TEXT NOT NULL,
+                width INTEGER NOT NULL,
+                height INTEGER NOT NULL
+            )
+        """.trimIndent()
+        )
+    }
 }

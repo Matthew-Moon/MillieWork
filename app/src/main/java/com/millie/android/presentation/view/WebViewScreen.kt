726017c911a6992be167fffef47b4d50469bc062
taeyoung.moon <taeyoung.moon@minishtech.com>
2025-05-12 22:30:01 +0900

    feat: object-fit, maximum 처리를 위한 중복코드 제거 및 함수화

--- app/src/main/java/com/millie/android/presentation/view/WebViewScreen.kt
+++ app/src/main/java/com/millie/android/presentation/view/WebViewScreen.kt
@@ -38,16 +38,12 @@ fun WebViewScreen(
         File(path).takeIf { it.exists() }
     }
 
-    val filePath = remember(catImage.id) {
+    val htmlContent = remember(catImage.id) {
         localFile?.let {
-            """
-                <html>
-                  <body style="margin:0;padding0;">
-                    <img src="file://${localFile.absolutePath}" style="width:100%;height:auto;" />
-                  </body>
-                </html>
-            """.trimIndent()
-        } ?: run { null }
+            imageHtml(url = it.absolutePath, isLocal = true)
+        } ?: run {
+            imageHtml(url = catImage.url, isLocal = false)
+        }
     }
 
     Scaffold(
@@ -76,13 +72,7 @@ fun WebViewScreen(
                         setSupportZoom(true)
                     }
 
-                    filePath?.let {
-                        Timber.d("### 로컬 이미지 ==> $it")
-                        loadDataWithBaseURL(null, it, "text/html", "utf-8", null)
-                    } ?: run {
-                        Timber.d("### 서버 이미지 ==> ${getHtmlContent(catImage.url)}")
-                        loadDataWithBaseURL(null, getHtmlContent(catImage.url), "text/html", "utf-8", null)
-                    }
+                    loadDataWithBaseURL(null, htmlContent, "text/html", "utf-8", null)
                 }
             },
             modifier = Modifier
@@ -97,9 +87,10 @@ fun WebViewScreen(
  * object-fit: cover 로 바꾸면 이미지가 잘릴 수 있지만 화면을 꽉 채웁니다.
  * object-fit: contain 은 이미지 전체를 보여주고 비율을 유지합니다.
  */
-private fun getHtmlContent(url: String): String {
-    return """              
-        <!DOCTYPE html>
+private fun imageHtml(url: String, isLocal: Boolean): String {
+    val src = if (isLocal) "file://$url" else url
+    return """
+         <!DOCTYPE html>
             <html>
             <head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=3.0, user-scalable=yes">
@@ -117,16 +108,13 @@ private fun getHtmlContent(url: String): String {
                     img {
                         width: 100vw;
                         height: 100vh;
-                        object-fit: contain;
+                        object-fit: cover;
                     }
                 </style>
             </head>
             <body>
-                <img src="$url" />
+                <img src="$src" />
             </body>
             </html>
     """.trimIndent()
 }
-
-// TODO html 서버와로컬 코드 공통 및 정렬 처리 필요함
-// 내부 파일 캐시 정책 검토 어떤게 효율적인지?? 일반적인 방법 찾기
